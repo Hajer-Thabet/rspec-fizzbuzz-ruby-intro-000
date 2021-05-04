e332a46c76307b76be29efb7efaa43ae78ed2883
Hajer Thabet <hajerthabet10@gmail.com>
2021-05-04 07:15:30 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -10,7 +10,7 @@ def fizzbuzz(int)
 
 
 
-  elsif int % 3 == 0 && int % 5 == 0 # if the number int is divisible by 3 and 5
+  elsif (int % 3 == 0) && (int % 5 == 0)# if the number int is divisible by 3 and 5
     fizz_15="FizzBuzz"# Go fizzbuzz
 
 
