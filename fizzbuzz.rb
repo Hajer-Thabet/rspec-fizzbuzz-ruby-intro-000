f30fc6285fcd8014d417c1ce69850a9b07439642
Hajer Thabet <hajerthabet10@gmail.com>
2021-05-04 07:32:16 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,8 +5,8 @@ def fizzbuzz(int)
     fizz_3="Fizz" # Go fizz
   elsif int % 5 == 0
     fizz_5="Buzz"
-  elsif int % 15 == 0
-    fizz_15="FizzBuzz"
+    elsif int % 15 == 0
+      fizz_15="FizzBuzz"
 
 
   end
